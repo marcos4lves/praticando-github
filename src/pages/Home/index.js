62e42bdb0ab56751df68a212c325a2b4e350efe3
marcosalves <marcos97alvess@gmail.com>
2023-06-09 03:40:02 -0300

    correção do text/ image

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Image, Text, StyleSheetCreate } from 'react-native';
+
+export default function Home(){
+    return(
+        <View>
+            <Text> Esse é o texto da página inicial, Home </Text>
+            <Text> Do Git e do Git Hub </Text>
+        </View>
+    )
+};
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+export default function SignUp(){
+    return(
+        <View>
+            <Text> Essa é a página de cadastro </Text>
+            <TouchableOpacity><Text> Cadastrar </Text></TouchableOpacity>
+        </View>
+    )
+}
